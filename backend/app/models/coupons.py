58b59b6bc89001632dfefafcd9271b03f7e93561
Pi-pu-pip <andruxa-mix112@yandex.ru>
2023-10-09 23:45:34 +0300

    add coupon class

--- backend/app/models/coupons.py
+++ backend/app/models/coupons.py
@@ -1,3 +1,18 @@
 import databases
 import sqlalchemy
 from pydantic import BaseModel
+
+class Coupon(Base):
+    __tablename__ = 'coupon'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    time = Column(DateTime, default=datetime.now)
+    window = Column(String, nullable=False)
+    active = Column(Boolean, default=True)
+
+# Создать базу данных в памяти
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+# Создать таблицы на основе моделей
+Base.metadata.create_all(engine)
