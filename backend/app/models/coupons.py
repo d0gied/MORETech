3c08c460814f8ee7b4b5013354e2b4f3633a8661
Pi-pu-pip <andruxa-mix112@yandex.ru>
2023-10-10 01:38:33 +0300

    add get func

--- backend/app/models/coupons.py
+++ backend/app/models/coupons.py
@@ -3,6 +3,11 @@ from sqlalchemy import *
 from datetime import datetime
 from pydantic import BaseModel
 
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, aliased
+
 class Coupon(Base):
     __tablename__ = 'coupon'
 
@@ -17,3 +22,25 @@ engine = create_engine('sqlite:///:memory:', echo=True)
 
 # Создать таблицы на основе моделей
 Base.metadata.create_all(engine)
+
+@app.get("/coupons/")
+def get_coupons(department_id: int, amount: int = 10, page: int = 0, db: SessionLocal = Depends(SessionLocal)):
+    offset = page * amount
+    coupons = (
+        db.query(Coupon.name, Coupon.time)
+        .filter(Coupon.department_id == department_id)
+        .limit(amount)
+        .offset(offset)
+        .all()
+    )
+    
+    total_coupons = db.query(func.count(Coupon.id)).filter(Coupon.department_id == department_id).scalar()
+    total_pages = (total_coupons + amount - 1) // amount
+
+    return {
+        "department_id": department_id,
+        "page": page,
+        "total_pages": total_pages,
+        "amount": len(coupons),
+        "coupons": [{"name": name, "time": time} for name, time in coupons]
+    }
