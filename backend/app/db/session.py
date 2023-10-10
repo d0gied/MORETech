19a79f20f70aac2aeea0cbae311c2cad0b56fd98
Pi-pu-pip <andruxa-mix112@yandex.ru>
2023-10-10 13:38:05 +0300

    add session

---
+++ backend/app/db/session.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+from typing import Generator
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from orders_api.config import get_settings
+
+engine = create_engine(get_settings().database_url, pool_pre_ping=True)
+
+@lru_cache
+def create_session() -> scoped_session:
+    Session = scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    )
+    return Session
+
+def get_session() -> Generator[scoped_session, None, None]:
+    Session = create_session()
+    try:
+        yield Session
+    finally:
+        Session.remove()
