84a4a0161fe84a63bab78f160fdaecbce49d5f27
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2023-10-10 14:40:11 +0300

    homepage structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,32 @@
-import { BottomToolbar, Button, Page } from 'react-onsenui';
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
+import { BottomToolbar, Button, Page } from "react-onsenui"
+import { YMaps, Map } from "@pbe/react-yandex-maps"
+import "onsenui/css/onsenui.css"
+import "onsenui/css/onsen-css-components.css"
 
 function App() {
-  return (
-    <div className="app">
-      <Page>
-        <Button onClick={() => {console.log("Hello!")}}> Tap me!</Button>
-        <BottomToolbar >test</BottomToolbar>
-      </Page>
-    </div>
-  )
+    return (
+        <div className="app">
+            <Page>
+                <YMaps>
+                    <div className="department-list-wrapper">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Minus non quo aspernatur culpa doloribus temporibus
+                        quae, architecto quidem provident delectus. Sit quas
+                        dicta nesciunt sunt hic nobis repudiandae dolorum.
+                        Doloribus.
+                    </div>
+                    <Map
+                        defaultState={{
+                            center: [55.75, 37.57],
+                            zoom: 9
+                        }}
+                        className="map-container"
+                    />
+
+                </YMaps>
+            </Page>
+        </div>
+    )
 }
 
 export default App
---
+++ client/src/components/sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function sidebar() {
+    return (
+        <div className="department-list-wrapper">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus non
+            quo aspernatur culpa doloribus temporibus quae, architecto quidem
+            provident delectus. Sit quas dicta nesciunt sunt hic nobis
+            repudiandae dolorum. Doloribus.
+        </div>
+    )
+}
