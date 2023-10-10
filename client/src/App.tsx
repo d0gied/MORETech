f01725a3f54a7782746185ca115104df30f391c8
PixelQuasar <47022046+PixelQuasar@users.noreply.github.com>
2023-10-10 13:30:53 +0300

    update dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import { BottomToolbar, Button, Page } from 'react-onsenui';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
+function App() {
   return (
     <div className="app">
-      demo
+      <Page>
+        <Button onClick={() => {console.log("Hello!")}}> Tap me!</Button>
+        <BottomToolbar >test</BottomToolbar>
+      </Page>
     </div>
   )
 }
